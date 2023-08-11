27957f73df0ecba0cb9e3fa43d6d39ab1d8f1b53
sreedhar <sreedhar70755@gmail.com>
2023-08-11 19:01:35 +0530

    tf

---
+++ shell_scripting/frontend.sh
@@ -0,0 +1,10 @@
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+
+rm -rf /usr/share/nginx/html*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
