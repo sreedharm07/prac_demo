9542ca0a0e687f7af8356574399eeafe6710f3ca
sreedhar <sreedhar70755@gmail.com>
2023-08-11 19:58:43 +0530

    tf

---
+++ shell_scripting/catalogue.sh
@@ -0,0 +1,18 @@
+cp catalogue.service  /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
+
+yum install mongodb-org-shell -y
+mongo --host 3.88.108.111 </app/schema/catalogue.js
