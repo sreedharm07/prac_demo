fae75d260ba2a9bc0d2d9167ec6e8ef4295f645e
sreedhar <sreedhar70755@gmail.com>
2023-08-12 06:55:33 +0530

    tf

--- shell_scripting/user.sh
+++ shell_scripting/user.sh
@@ -4,7 +4,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-rm -rf app
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
