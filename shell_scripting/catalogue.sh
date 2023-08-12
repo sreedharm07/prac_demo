7eea671f5233c36a8fc613442cb653b7128c138b
sreedhar <sreedhar70755@gmail.com>
2023-08-12 17:41:20 +0530

    tf

--- shell_scripting/catalogue.sh
+++ shell_scripting/catalogue.sh
@@ -4,6 +4,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
