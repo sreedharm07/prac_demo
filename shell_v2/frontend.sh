8dd5781169a45df96535cad99b57790fb9322d28
sreedhar <sreedhar70755@gmail.com>
2023-08-12 20:28:07 +0530

    tf

--- shell_v2/frontend.sh
+++ shell_v2/frontend.sh
@@ -1,19 +1,19 @@
 echo ">>>>>>>>>>>>>>>>copying the config>>>>>>>>>>>>>>>>>>>>>>"
-cp nginx.conf /etc/nginx/default.d/roboshop.conf
+cp nginx.conf /etc/nginx/default.d/roboshop.conf  &>> /tmp/roboshop.log
 
 echo ">>>>>>>>>>>>>>>>installing nginx>>>>>>>>>>>>>>>>>>>>>>"
-yum install nginx -y
-rm -rf /usr/share/nginx/html/*
+yum install nginx -y               &>> /tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*     &>> /tmp/roboshop.log
 
 echo ">>>>>>>>>>>>>>>>downloading app>>>>>>>>>>>>>>>>>>>>>>"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip     &>> /tmp/roboshop.log
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html        &>> /tmp/roboshop.log
 
 echo ">>>>>>>>>>>>>>>>unzipping app>>>>>>>>>>>>>>>>>>>>>>"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip             &>> /tmp/roboshop.log
 
 echo ">>>>>>>>>>>>>>>>systemctl commands>>>>>>>>>>>>>>>>>>>>>>"
 systemctl enable nginx
-systemctl start nginx
-systemctl restart nginx
+systemctl start nginx                  &>> /tmp/roboshop.log
+systemctl restart nginx                &>> /tmp/roboshop.log
