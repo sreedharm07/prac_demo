840ba269df38668a88bde15908d3326df9d9f336
sreedhar <sreedhar70755@gmail.com>
2023-08-12 22:24:43 +0530

    tf

--- shell_v2/common.sh
+++ shell_v2/common.sh
@@ -16,13 +16,37 @@ function_nodejs() {
   systemctl daemon-reload
   systemctl start ${component}
   systemctl enable ${component}
-  schema_function
+  function_schema
 }
 #------------------------------------------------------------------------------------------------------
-schema_function () {
+function_schema () {
   if [ "${schema_type}" == "mongodb" ]; then
   yum install mongodb-org-shell -y
   mongo --host 172.31.81.91 </app/schema/${component}.js
   fi
+
+  if [ "${schema_type}" == "mysql" ]; then
+  yum install mysql -y
+  mysql -h 172.31.93.1 -uroot -pRoboShop@1 < /app/schema/${component}.sql
+  fi
 }
 #--------------------------------------------------------------------------------------------------------
+function_maven(){
+  cp ${component}.service /etc/systemd/system/${component}.service
+
+  yum install maven -y
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+  unzip /tmp/${component}.zip
+  cd /app
+  mvn clean package
+  mv target/shipping-1.0.jar ${component}.jar
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl start ${component}
+  function_schema
+  systemctl restart ${component}
+}
--- shell_v2/shipping.sh
+++ shell_v2/shipping.sh
@@ -1,19 +1,5 @@
-cp shipping.service /etc/systemd/system/shipping.service
-
-yum install maven -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
-yum install mysql -y
-mysql -h 172.31.84.121 -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl restart shipping
+component=shipping
+schema_type=mysql
+source common.sh
+function_maven
 
