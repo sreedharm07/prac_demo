938452ae50ae63b2893fe037bee9867646a68f13
sreedhar <sreedhar70755@gmail.com>
2023-08-12 20:51:19 +0530

    tf

--- shell_v2/catalogue.sh
+++ shell_v2/catalogue.sh
@@ -1,20 +1,3 @@
-cp catalogue.service  /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-
-systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
-
-yum install mongodb-org-shell -y
-mongo --host 172.31.90.137 </app/schema/catalogue.js
+component = catalogue
+source = common.sh
+function_nodejs
---
+++ shell_v2/common.sh
@@ -0,0 +1,22 @@
+function_nodejs() {
+  cp ${component}.service  /etc/systemd/system/${component}.service
+  cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  yum install nodejs -y
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+  unzip /tmp/${component}.zip
+  cd /app
+  npm install
+
+  systemctl daemon-reload
+  systemctl start ${component}
+  systemctl enable ${component}
+
+  yum install mongodb-org-shell -y
+  mongo --host 172.31.90.137 </app/schema/${component}.js
+}
