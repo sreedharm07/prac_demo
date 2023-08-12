a42fc8fe34099a782e239960c53889564c8dc29e
sreedhar <sreedhar70755@gmail.com>
2023-08-12 20:36:29 +0530

    tf

--- shell_v2/frontend.sh
+++ shell_v2/frontend.sh
@@ -1,19 +1,19 @@
-echo ">>>>>>>>>>>>>>>>copying the config>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\e[32m>>>>>> copying the config file <<<<<<<<<<\e[0m"
 cp nginx.conf /etc/nginx/default.d/roboshop.conf  &>> /tmp/roboshop.log
 
-echo ">>>>>>>>>>>>>>>>installing nginx>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\e[31m>>>>>>>>>>>>>installing >>>>>>>>>>>>\e[0m"
 yum install nginx -y               &>> /tmp/roboshop.log
 rm -rf /usr/share/nginx/html/*     &>> /tmp/roboshop.log
 
-echo ">>>>>>>>>>>>>>>>downloading app>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\e[31m>>>>>>>>>>>>>>>>downloading app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip     &>> /tmp/roboshop.log
 
 cd /usr/share/nginx/html        &>> /tmp/roboshop.log
 
-echo ">>>>>>>>>>>>>>>>unzipping app>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\e[31m>>>>>>>>>>>>>>>>unzipping app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 unzip /tmp/frontend.zip             &>> /tmp/roboshop.log
 
-echo ">>>>>>>>>>>>>>>>systemctl commands>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\e[31m>>>>>>>>>>>>>>>>systemctl commands>>>>>>>>>>>>>>>>>>>>>>[0m"
 systemctl enable nginx
 systemctl start nginx                  &>> /tmp/roboshop.log
 systemctl restart nginx                &>> /tmp/roboshop.log
