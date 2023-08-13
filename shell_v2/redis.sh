19d57e1daeb16700558baf768ae3c0ae9bd9a9b0
sreedhar <sreedhar70755@gmail.com>
2023-08-13 08:12:57 +0530

    tf

--- shell_v2/redis.sh
+++ shell_v2/redis.sh
@@ -3,7 +3,7 @@ yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>>log
  function_status
 yum module enable redis:remi-6.2 -y   &>>log
  function_status
-yum install red -y   &>>log
+yum install redis -y   &>>log
  function_status
 systemctl enable redis   &>>log
 function_status
