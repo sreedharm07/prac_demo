be01ab720698215f3ad4abb67a19f56a1c604505
sreedhar <sreedhar70755@gmail.com>
2023-08-13 07:47:23 +0530

    tf

--- shell_v2/shipping.sh
+++ shell_v2/shipping.sh
@@ -1,24 +1,53 @@
-  cp shipping.service /etc/systemd/system/shipping.service   
+component=shipping
+function_status(){
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccess\e[0m"
+      else
+            echo -e "\e[31mfailure\e[0m"
+  fi
+  }
 
-  yum install maven -y    
+  cp ${component}.service /etc/systemd/system/${component}.service
+   function_status
+
+      echo -e "\e[36m--installing app------------\e[0m"
+  yum install maven -y
+   function_status
   id roboshop
   if [ $? != 0 ]; then
-        useradd roboshop    
+        useradd roboshop
+         function_status
   fi
-  rm -rf /app    
-  mkdir /app    
+  rm -rf /app
+  function_status
+  mkdir /app
+  function_status
         echo -e "\e[36m--installing microservice--\e[0m"
-  curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip     
-  cd /app    
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+   function_status
+  cd /app
+   function_status
         echo -e "\e[36m--unzipping--\e[0m"
-  unzip /tmp/shipping.zip    
-  cd /app    
-  mvn clean package     
-  mv target/shipping-1.0.jar shipping.jar
+  unzip /tmp/${component}.zip
+   function_status
+  cd /app
+    function_status
+  mvn clean package
+    function_status
+  rm -rf shipping.jar
+  mv target/shipping-1.0.jar ${component}.jar
+   function_status
         echo -e "\e[36m--systemctl commands--\e[0m"
-  systemctl daemon-reload    
-  systemctl enable shipping    
-  systemctl start shipping   
-  function_schema   
-  systemctl restart shipping     
+  systemctl daemon-reload
+  function_status
+  systemctl enable ${component}
+    function_status
+  systemctl start ${component}
+   function_status
+  yum install mysql -y
+   function_status
+    mysql -h 172.31.93.1 -uroot -pRoboShop@1 < /app/schema/${component}.sql
+  function_status
+  systemctl restart ${component}
+     function_status
 }
