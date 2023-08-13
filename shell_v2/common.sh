82b3fe6e5409d0f2c6bcf8bd5a4240932109fe23
sreedhar <sreedhar70755@gmail.com>
2023-08-13 07:06:08 +0530

    tf

--- shell_v2/common.sh
+++ shell_v2/common.sh
@@ -67,7 +67,7 @@ function_maven(){
   cp ${component}.service /etc/systemd/system/${component}.service    &>>$log | function_status
 
       echo -e "\e[36m--installing app--\e[0m"
-  yum install mav -y    &>>$log | function_status
+  yum install ma -y    &>>$log | function_status
   useradd roboshop    &>>$log | function_status
   rm -rf /app    &>>$log | function_status
   mkdir /app    &>>$log | function_status
