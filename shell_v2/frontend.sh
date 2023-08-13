4ee7943901aae3daf468b9fc3f9b74b195d868b0
sreedhar <sreedhar70755@gmail.com>
2023-08-13 05:48:38 +0530

    tf

--- shell_v2/frontend.sh
+++ shell_v2/frontend.sh
@@ -1,21 +1,38 @@
 log=/tmp/roboshop.log
+function_status() {
+  if [ $? == 0]; then
+      echo -e "\e[31msucess\e[0m"
+    else
+      echo -e "\e[32mfailure\e[0m"
+fi
+}
 
-echo -e "\e[32m>>>>>> copying the config file <<<<<<<<<<\e[0m"
-cp nginx.conf /etc/nginx/default.d/roboshop.conf  #&>> $log
+echo -e "\e[35m>>>>>> copying the config file <<<<<<<<<<\e[0m"
+cp nginx.conf /etc/nginx/default.d/roboshop.conf  &>> $log
+function_status
 
-echo -e "\e[31m>>>>>>>>>>>>>installing >>>>>>>>>>>>\e[0m"
-yum install nginx -y               #&>> $log
-rm -rf /usr/share/nginx/html/*     #&>> $log
+echo -e "\e[35m>>>>>>>>>>>>>installing >>>>>>>>>>>>\e[0m"
+yum install nginx -y               &>> $log
+function_status
+rm -rf /usr/share/nginx/html/*     &>> $log
+function_status
 
-echo -e "\e[31m>>>>>>>>>>>>>>>>downloading app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip     #&>> $log
+echo -e "\e[35m>>>>>>>>>>>>>>>>downloading app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip     &>> $log
+function_status
 
-cd /usr/share/nginx/html        #&>> $log
+cd /usr/share/nginx/html        &>> $log
+function_status
 
-echo -e "\e[31m>>>>>>>>>>>>>>>>unzipping app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-unzip /tmp/frontend.zip             #&>> $log
+echo -e "\e[35m>>>>>>>>>>>>>>>>unzipping app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+unzip /tmp/frontend.zip             &>> $log
+function_status
 
-echo -e "\e[31m>>>>>>>>>>>>>>>>systemctl commands>>>>>>>>>>>>>>>>>>>>>>[0m"
+echo -e "\e[35m>>>>>>>>>>>>>>>>systemctl commands>>>>>>>>>>>>>>>>>>>>>>[0m"
 systemctl enable nginx
-systemctl start nginx                  #&>> $log
-systemctl restart nginx                #&>> $log
+function_status
+systemctl start nginx                  &>> $log
+function_status
+systemctl restart nginx                &>> $log
+function_status
+
