07c6b6976133460b582810a3231dbf8127c1725d
sreedhar <sreedhar70755@gmail.com>
2023-08-13 08:19:06 +0530

    tf

--- shell_v2/rabbitmq.sh
+++ shell_v2/rabbitmq.sh
@@ -1,18 +1,18 @@
 source common.sh
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash    &>>log
+curl -s ps://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash    &>>$log
  function_status
-curl -s ht://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash   &>>log
+curl -s ht://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash   &>>$log
  function_status
 
-yum install rabbitmq-server -y   &>>log
+yum install rabbitmq-server -y   &>>$log
 function_status
 
-systemctl enable rabbitmq-server   &>>log
+systemctl enable rabbitmq-server   &>>$log
  function_status
-systemctl start rabbitmq-server    &>>log
+systemctl start rabbitmq-server    &>>$log
  function_status
 
-rabbitmqctl add_user roboshop roboshop123     &>>log
+rabbitmqctl add_user roboshop roboshop123     &>>$log
  function_status
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>>log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>>$log
  function_status
