519eed918d3ff86c03f18735f1895d4b8037a942
sreedhar <sreedhar70755@gmail.com>
2023-08-13 06:49:26 +0530

    tf

--- shell_v2/common.sh
+++ shell_v2/common.sh
@@ -2,44 +2,44 @@ log=/tmp/roboshop.log
 
 function_nodejs() {
   echo -e "\e[36m--copying--\e[0m"
-  cp ${component}.service  /etc/systemd/system/${component}.service   &>>log
+  cp ${component}.service  /etc/systemd/system/${component}.service   &>>$log
   function_status
-  cp mongo.repo /etc/yum.repos.d/mongo.repo          &>>log
+  cp mongo.repo /etc/yum.repos.d/mongo.repo          &>>$log
    function_status
 
   echo -e "\e[36m--downloading repos--\e[0m"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash    &>>log
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash    &>>$log
    function_status
 
     echo -e "\e[36m--installing--\e[0m"
-  yum install no -y   &>>log
+  yum install no -y   &>>$log
    function_status
-  useradd roboshop    &>>log
+  useradd roboshop    &>>$log
    function_status
-  rm -rf /app       &>>log
+  rm -rf /app       &>>$log
   function_status
-  mkdir /app        &>>log
+  mkdir /app        &>>$log
    function_status
     echo -e "\e[36m--downloading--\e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip     &>>log
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip     &>>$log
    function_status
 
-  cd /app      &>>log
+  cd /app      &>>$log
    function_status
     echo -e "\e[36m--unzipping--\e[0m"
-  unzip /tmp/${component}.zip     &>>log
+  unzip /tmp/${component}.zip     &>>$log
    function_status
-  cd /app     &>>log
+  cd /app     &>>$log
    function_status
-  npm install     &>>log
+  npm install     &>>$log
    function_status
 
   echo -e "\e[36m--systemctl commands--\e[0m"
-  systemctl daemon-reload    &>>log
+  systemctl daemon-reload    &>>$log
    function_status
-  systemctl start ${component}    &>>log
+  systemctl start ${component}    &>>$log
    function_status
-  systemctl enable ${component}     &>>log
+  systemctl enable ${component}     &>>$log
    function_status
   function_schema
 }
@@ -47,87 +47,87 @@ function_nodejs() {
 function_schema () {
     echo -e "\e[36m--mongoodb schema--\e[0m"
   if [ "${schema_type}" == "mongodb" ]; then
-  yum install mongodb-org-shell -y      &>>log | function_status
-  mongo --host 172.31.81.91 </app/schema/${component}.js      &>>log | function_status
+  yum install mongodb-org-shell -y      &>>$log | function_status
+  mongo --host 172.31.81.91 </app/schema/${component}.js      &>>$log | function_status
   fi
     echo -e "\e[36m--mysql schema--\e[0m"
   if [ "${schema_type}" == "mysql" ]; then
-  yum install mysql -y     &>>log | function_status
-  mysql -h 172.31.93.1 -uroot -pRoboShop@1 < /app/schema/${component}.sql     &>>log | function_status
+  yum install mysql -y     &>>$log | function_status
+  mysql -h 172.31.93.1 -uroot -pRoboShop@1 < /app/schema/${component}.sql     &>>$log | function_status
   fi
 }
 #--------------------------------------------------------------------------------------------------------
 function_maven(){
       echo -e "\e[36m--copying--\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service    &>>log | function_status
+  cp ${component}.service /etc/systemd/system/${component}.service    &>>$log | function_status
 
       echo -e "\e[36m--installing app--\e[0m"
-  yum install maven -y    &>>log | function_status
-  useradd roboshop    &>>log | function_status
-  rm -rf /app    &>>log | function_status
-  mkdir /app    &>>log | function_status
+  yum install maven -y    &>>$log | function_status
+  useradd roboshop    &>>$log | function_status
+  rm -rf /app    &>>$log | function_status
+  mkdir /app    &>>$log | function_status
         echo -e "\e[36m--installing microservice--\e[0m"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip     &>>log | function_status
-  cd /app    &>>log | function_status
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip     &>>$log | function_status
+  cd /app    &>>$log | function_status
         echo -e "\e[36m--unzipping--\e[0m"
-  unzip /tmp/${component}.zip    &>>log | function_status
-  cd /app    &>>log | function_status
-  mvn clean package     &>>log | function_status
-  mv target/shipping-1.0.jar ${component}.jar    &>>log | function_status
+  unzip /tmp/${component}.zip    &>>$log | function_status
+  cd /app    &>>$log | function_status
+  mvn clean package     &>>$log | function_status
+  mv target/shipping-1.0.jar ${component}.jar    &>>$log | function_status
         echo -e "\e[36m--systemctl commands--\e[0m"
-  systemctl daemon-reload    &>>log | function_status
-  systemctl enable ${component}    &>>log | function_status
-  systemctl start ${component}   &>>log | function_status
-  function_schema   &>>log | function_status
-  systemctl restart ${component}     &>>log | function_status
+  systemctl daemon-reload    &>>$log | function_status
+  systemctl enable ${component}    &>>$log | function_status
+  systemctl start ${component}   &>>$log | function_status
+  function_schema   &>>$log | function_status
+  systemctl restart ${component}     &>>$log | function_status
 }
 #-----------------------------------------------------------------------------------------------------------
 function_python(){
         echo -e "\e[36m--copying--\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service   &>>log | function_status
+  cp ${component}.service /etc/systemd/system/${component}.service   &>>$log | function_status
       echo -e "\e[36m--installing python--\e[0m"
-  yum install python36 gcc python3-devel -y    &>>log | function_status
-  useradd roboshop    &>>log | function_status
-  rm -rf /app    &>>log | function_status
-  mkdir /app     &>>log | function_status
+  yum install python36 gcc python3-devel -y    &>>$log | function_status
+  useradd roboshop    &>>$log | function_status
+  rm -rf /app    &>>$log | function_status
+  mkdir /app     &>>$log | function_status
         echo -e "\e[36m--downloading service--\e[0m"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip    &>>log | function_status
-  cd /app    &>>log | function_status
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip    &>>$log | function_status
+  cd /app    &>>$log | function_status
         echo -e "\e[36m--unzipping service--\e[0m"
-  unzip /tmp/payment.zip    &>>log | function_status
-  cd /app    &>>log | function_status
-  pip3.6 install -r requirements.txt    &>>log | function_status\
+  unzip /tmp/payment.zip    &>>$log | function_status
+  cd /app    &>>$log | function_status
+  pip3.6 install -r requirements.txt    &>>$log | function_status\
         echo -e "\e[36m--systemctl commands--\e[0m"
-  systemctl daemon-reload    &>>log | function_status
-  systemctl enable ${component}    &>>log | function_status
-  systemctl start ${component}    &>>log | function_status
+  systemctl daemon-reload    &>>$log | function_status
+  systemctl enable ${component}    &>>$log | function_status
+  systemctl start ${component}    &>>$log | function_status
 }
 #-----------------------------------------------------------------------------------------------------------
 function_golang(){
         echo -e "\e[36m--copying repos--\e[0m"
-cp ${component}.service /etc/systemd/system/${component}.service    &>>log | function_status
+cp ${component}.service /etc/systemd/system/${component}.service    &>>$log | function_status
       echo -e "\e[36m--installing golang--\e[0m"
-yum install golang -y     &>>log | function_status
-useradd roboshop    &>>log | function_status
-rm -rf /app     &>>log | function_status
-mkdir /app     &>>log | function_status
+yum install golang -y     &>>$log | function_status
+useradd roboshop    &>>$log | function_status
+rm -rf /app     &>>$log | function_status
+mkdir /app     &>>$log | function_status
       echo -e "\e[36m--downloading service--\e[0m"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip     &>>log | function_status
-cd /app     &>>log | function_status
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip     &>>$log | function_status
+cd /app     &>>$log | function_status
       echo -e "\e[36m--unzipping service--\e[0m"
-unzip /tmp/${component}.zip     &>>log | function_status
-cd /app     &>>log | function_status
-go mod init ${component}     &>>log | function_status
-go get     &>>log | function_status
-go build     &>>log | function_status
+unzip /tmp/${component}.zip     &>>$log | function_status
+cd /app     &>>$log | function_status
+go mod init ${component}     &>>$log | function_status
+go get     &>>$log | function_status
+go build     &>>$log | function_status
       echo -e "\e[36m--systemctl commands--\e[0m"
-systemctl daemon-reload     &>>log | function_status
-systemctl enable ${component}      &>>log | function_status
-systemctl start ${component}       &>>log | function_status
+systemctl daemon-reload     &>>$log | function_status
+systemctl enable ${component}      &>>$log | function_status
+systemctl start ${component}       &>>$log | function_status
 }
 #-----------------------------------------------------------------------------------------------------------
 function_status(){
-  if [ $? == 0 ]; then
+  if [ $? -eq 0 ]; then
     echo -e "\e[32m---success---\e[0m"
     else
       echo -e "\e[31m---failure--\e[om"
