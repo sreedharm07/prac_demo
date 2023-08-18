a2e4c7e3ec3d0a3d5fdca0673c082eeccb9e663d
sreedhar <sreedhar70755@gmail.com>
2023-08-18 11:02:48 +0530

    tf

--- tera/main.tf
+++ tera/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  security_groups=  [var.security_group]
+  ami            = data.aws_ami.ami.id
+  instance_type  = var.instance_type
+  vpc_security_group_ids =  [var.security_group]
 
-  for_each = var.components
+  for_each        = var.components
   tags = {
-    Name = each.key
+    Name           = each.key
   }
 }
