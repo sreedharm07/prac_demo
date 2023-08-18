0082cfb72df8db3fa4bbed929db74526a48fe0f2
sreedhar <sreedhar70755@gmail.com>
2023-08-18 13:55:32 +0530

    tf

--- tera/modules/main.tf
+++ tera/modules/main.tf
@@ -8,7 +8,6 @@ resource "aws_instance" "instance" {
   }
 }
 
-
 resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   name    = "${var.components}.cloudev7.online"
@@ -18,4 +17,17 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 }
 
+resource "null_resource" "ansible" {
+depends_on = [aws_route53_record.record ]
 
+  provisioner "local-exec" {
+    command = <<EOF
+
+cd /home/centos/learn-ansible
+git pull
+sleep 30
+playbook-ansible -i "${var.components}-dev.cloudev7.online", main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component="${var.components}"
+
+EOF
+  }
+}
