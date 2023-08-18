ef632a2c386aaafae755e4682d46d070619c33ad
sreedhar <sreedhar70755@gmail.com>
2023-08-18 14:03:38 +0530

    tf

--- tera/modules/main.tf
+++ tera/modules/main.tf
@@ -26,7 +26,7 @@ depends_on = [aws_route53_record.record ]
 cd /home/centos/learn-ansible
 git pull
 sleep 30
-playbook-ansible -i ${var.components}-dev.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.components}
+ansible-playbook -i ${var.components}-dev.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.components}
 
 EOF
   }
