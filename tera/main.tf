df8fbcedade1848c10d0c8250c75f001e6be87e2
sreedhar <sreedhar70755@gmail.com>
2023-08-18 11:23:04 +0530

    tf

--- tera/main.tf
+++ tera/main.tf
@@ -10,8 +10,9 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record" {
+  for_each = var.components
   zone_id = var.zone_id
-  name    = "${var.components}-dev"
+  name    = "${each.key}-dev"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
