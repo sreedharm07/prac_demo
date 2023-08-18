77efe0170e9a952072216f684a836bab57243a5e
sreedhar <sreedhar70755@gmail.com>
2023-08-18 12:23:00 +0530

    tf

--- tera/vars.tf
+++ tera/vars.tf
@@ -2,3 +2,24 @@
 #variable "security_group" {}
 #variable "components" {}
 #variable "zone_id" {}
+
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "security_group" {
+  default = "sg-03c71c5d008981a14"
+}
+variable "components" {
+  default = "Z07380001ED1GOXY0KMLD"
+}
+variable "zone_id" {
+  default = {
+    components = {
+      frontend  = { name = "frontend" }
+      mongodb   = { name = "mongodb" }
+      catalogue = { name = "catalogue" }
+      redis     = { name = "redis" }
+    }
+  }
+}
