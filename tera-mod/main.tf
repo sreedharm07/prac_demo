dbe71ab9c06c160ccc45ad624016e11811cf1135
sreedhar <sreedhar70755@gmail.com>
2023-08-20 07:42:29 +0530

    tf

--- tera-mod/main.tf
+++ tera-mod/main.tf
@@ -2,3 +2,13 @@ resource "aws_vpc" "vpc" {
   for_each = var.vpc
   cidr_block = each.value["cidr"]
 }
+
+resource "aws_subnet" "main" {
+  for_each = var.vpc["subnets"]
+  vpc_id     = aws_vpc.vpc
+  cidr_block = each.value["cidr"]
+
+  tags = {
+    Name = each.key
+  }
+}
--- tera-mod/mod/varr.tfvars
+++ tera-mod/mod/varr.tfvars
@@ -1,8 +1,11 @@
 vpc={
   main={
     cidr= "10.0.0.0/16"
-  }
-  demo={
-    cidr="10.1.0.0/24"
+    subnets={
+      public={
+        pb1={cidr="10.0.0.0/24" , az= "us-east-1a"}
+        pb2={cidr="10.0.1.0/24" , az= "us-east-1b"}
+      }
+    }
   }
 }
