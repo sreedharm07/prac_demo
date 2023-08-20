768534c1ff5b01922a25109ace55d30c5027ed48
sreedhar <sreedhar70755@gmail.com>
2023-08-20 10:05:34 +0530

    tf

--- tera-mod/main.tf
+++ tera-mod/main.tf
@@ -2,6 +2,6 @@ module "vpc" {
   source = "git::https://github.com/sreedharm07/tera-vpc.git"
 
   for_each = var.vpc
-  cidr=each.value["cidr"]
-  subnets=each.value["subnets"]
+  cidr= each.value["cidr"]
+  subnets= each.value["subnets"]
 }
