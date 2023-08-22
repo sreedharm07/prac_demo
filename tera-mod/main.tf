cb65f49e664c3d7feffcae4dd16daa49416e9397
sreedhar <sreedhar70755@gmail.com>
2023-08-22 08:08:02 +0530

    tf

--- tera-mod/main.tf
+++ tera-mod/main.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr=each.value["cidr"]
   subnets=each.value["subnets"]
   default_vpc=var.default_vpc
-  default_route_id=var.default_route_id
-  default_vpc_route_id=default_vpc_route_id
+  default_vpc_cidr=var.default_vpc_cidr
+  default_vpc_route_id=var.default_vpc_route_id
 }
 
 
