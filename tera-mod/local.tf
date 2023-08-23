35962a2d5b2036fea3ef2d605702637c87670c31
sreedhar <sreedhar70755@gmail.com>
2023-08-23 11:53:24 +0530

    tf

--- tera-mod/local.tf
+++ tera-mod/local.tf
@@ -1,5 +1,5 @@
 locals {
-  vpc_id= lookup(lookup(module.to_vpc, "main", null), "id", null)
+  vpc_id= lookup(lookup(module.to_vpc, "main", null), "vpc_id", null)
 
   subnets-ids = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"app",null),"subnets",null) : v.id]
 
