a829a7e5faa0f681f2d71d3f90307ea644d6b98b
sreedhar <sreedhar70755@gmail.com>
2023-08-23 10:03:35 +0530

    tf

--- tera-mod/local.tf
+++ tera-mod/local.tf
@@ -1,7 +1,7 @@
 locals {
   vpc_id=lookup(lookup(module.to_vpc, "main", null), "vpc_id", null)
 
-  subnets-ids = lookup(module.to_vpc, "main",null ),"subnet",null),
+#  subnets-ids = lookup(module.to_vpc, "main",null ),"subnet",null),
 
 
 
