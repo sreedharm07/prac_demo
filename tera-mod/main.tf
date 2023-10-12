e21fa4ea7e926dae950eee25e0514d2b5068c616
sreedhar <sreedhar70755@gmail.com>
2023-10-12 20:32:30 +0530

    tf

--- tera-mod/main.tf
+++ tera-mod/main.tf
@@ -142,7 +142,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "prod-roboshop"
   cluster_version = "1.28"
 
   cluster_endpoint_public_access = false
