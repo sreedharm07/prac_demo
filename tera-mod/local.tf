22c5fe375dcfa4730bf02a5973a1b2c0f9199ca4
sreedhar <sreedhar70755@gmail.com>
2023-10-12 10:58:45 +0530

    tf

--- tera-mod/local.tf
+++ tera-mod/local.tf
@@ -7,10 +7,10 @@ locals {
   subnets-apps-cidr = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"app",null),"subnets",null) : v.cidr_block]
 
 
-  dns_name= lookup(lookup(lookup(module.alb,"private",null ),"alb",null),"dns_name",null)
-  dns_public_name= lookup(lookup(lookup(module.alb,"public",null ),"alb",null),"dns_name",null)
-
-  listner= lookup(lookup(lookup(module.alb,"private",null ),"listner",null),"arn",null)
-  public_listner= lookup(lookup(lookup(module.alb,"public",null ),"listner",null),"arn",null)
+#  dns_name= lookup(lookup(lookup(module.alb,"private",null ),"alb",null),"dns_name",null)
+#  dns_public_name= lookup(lookup(lookup(module.alb,"public",null ),"alb",null),"dns_name",null)
+#
+#  listner= lookup(lookup(lookup(module.alb,"private",null ),"listner",null),"arn",null)
+#  public_listner= lookup(lookup(lookup(module.alb,"public",null ),"listner",null),"arn",null)
 
 }
