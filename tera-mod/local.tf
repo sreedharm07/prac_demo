edbffd16230d999f7b892ba297fd9bfaa51f4035
91707 <sreedhar70755@gmail.com>
2023-12-20 10:18:35 +0530

    tf

--- tera-mod/local.tf
+++ tera-mod/local.tf
@@ -1,10 +1,10 @@
-locals {
-  vpc_id= lookup(lookup(module.to_vpc, "main", null), "vpc_id", null)
-
-  subnets-ids = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"app",null),"subnets",null) : v.id]
-  db-ids = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"db",null),"subnets",null) : v.id]
-
-  subnets-apps-cidr = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"app",null),"subnets",null) : v.cidr_block]
+#locals {
+#  vpc_id= lookup(lookup(module.to_vpc, "main", null), "vpc_id", null)
+#
+#  subnets-ids = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"app",null),"subnets",null) : v.id]
+#  db-ids = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"db",null),"subnets",null) : v.id]
+#
+#  subnets-apps-cidr = [for k,v in lookup(lookup(lookup(lookup(module.to_vpc, "main",null ),"subnet",null),"app",null),"subnets",null) : v.cidr_block]
 
 
 #  dns_name= lookup(lookup(lookup(module.alb,"private",null ),"alb",null),"dns_name",null)
